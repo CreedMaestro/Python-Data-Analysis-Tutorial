570af1a58330807dbd95ef885ee4989118cbf243
CreedMaestro <46832867+CreedMaestro@users.noreply.github.com>
2022-10-24 17:49:26 +0900

    python_function

---
+++ python_function.py
@@ -0,0 +1,46 @@
+
+# def name(input):
+#     print("제 이름은 " + input + "입니다.")
+
+# name("크리드")
+
+# def sum(a,b):
+#     print('a+b')
+#     print(a+b)
+#     return a+b   #함수의 아웃풋 값
+
+# total = sum(3,4)
+# print('total')
+# print(total)
+
+# Q. 성적과 이름을 입력하고, 성적에 따른 등급을 알려주는 프로그램을 만드시오. [함수를 사용]
+
+# 90점 이상 A
+# 80점 이상 B
+# 70점 이상 C
+# 60점 이상 D
+# 60점 미만 E
+
+# grade = ""
+# def check_grade(성적, 이름):
+#     if 성적 >=90:
+#         print(이름 + "의 등급은 A입니다.")
+#     elif 성적 >=80:
+#         print(이름 + "의 등급은 B입니다.")
+#     elif 성적 >=70:
+#         print(이름 + '의 등급은 C입니다.')
+#     elif 성적 >=60:
+#         print(이름 + "의 등급은 D입니다.")
+#     elif 성적 < 60:
+#         print(이름 + "의 등급은 E입니다.")
+
+#     return grade
+
+import random
+# b = random.random(1,10)
+# print(b)
+
+# a = [1, 2, 3]
+# random.shuffle(a)
+# print(a)
+
